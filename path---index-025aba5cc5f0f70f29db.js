f6575815ac80d160daf41c4e71f6190b79d0f322
Denis Nazarov <dnazarov@users.noreply.github.com>
2019-01-10 15:50:36 -0800

    Updates

--- path---index-025aba5cc5f0f70f29db.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x81b8806e4260],{343:function(t,e){t.exports={data:{allContentJson:{edges:[{node:{index:{title:"Hello World",subtitle:"This is a simple gatsby-starter"}}}]}},pathContext:{}}}});
-//# sourceMappingURL=path---index-025aba5cc5f0f70f29db.js.map
